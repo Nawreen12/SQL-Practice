668077028a11948c2a31dc62fc991392c61c3d67
Nawreen Afaz Chaity <nawreen11@gmail.com>
2025-06-12 23:17:32 +0100

    Add files via upload

---
+++ Select Statement/Select Statement.sql
@@ -0,0 +1,39 @@
+Select* from dbo.EmployeeDemographics;
+
+
+
+Select Top 5* from dbo.EmployeeDemographics;
+
+
+
+
+Select Distinct (EmployeeID) from dbo.EmployeeDemographics;
+
+
+
+
+Select Count (FirstName) From dbo.EmployeeDemographics;
+
+
+
+
+
+Select Max (Salary) from dbo.EmployeeSalary;
+
+
+
+
+
+Select MIN (Salary) from dbo.EmployeeSalary;
+
+
+
+
+
+
+Select AVG (Salary) from dbo.EmployeeSalary;
+
+
+
+
+
