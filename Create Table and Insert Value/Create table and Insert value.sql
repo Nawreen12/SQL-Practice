d719b31600f744a9ce319ff9c8b2b427d19a2cf5
Nawreen Afaz Chaity <nawreen11@gmail.com>
2025-06-12 20:08:21 +0100

    Add files via upload

---
+++ Create Table and Insert Value/Create table and Insert value.sql
@@ -0,0 +1,7 @@
+Create table Employee (EmployeeID int, FirstName varchar (50), LastName varchar (50), Age int, Gender varchar (50) );
+
+
+
+
+Insert INTO Employee Values ('1001', 'Nawreen', 'Afaz', '32','Female');
+
