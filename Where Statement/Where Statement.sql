3efbdbf2102ad4c97791df20e372ccdf12bc88d2
Nawreen Afaz Chaity <nawreen11@gmail.com>
2025-06-13 22:30:13 +0100

    Add files via upload

---
+++ Where Statement/Where Statement.sql
@@ -0,0 +1,36 @@
+Select * From dbo.EmployeeDemographics;
+
+
+
+Select *From dbo.EmployeeDemographics Where Age = '30';
+
+
+
+Select * From dbo.EmployeeDemographics where Age <> '30';
+
+
+
+Select * From dbo.EmployeeDemographics where Age > '30';
+
+
+
+Select * From dbo.EmployeeDemographics where Age < '30';
+
+
+
+Select * From dbo.EmployeeDemographics where Age >= '30';
+
+
+
+Select * From dbo.EmployeeDemographics where Age >'30' and  Gender = 'Female';
+
+
+
+Select * From dbo.EmployeeDemographics where Age >'30' or Gender = 'Female';
+
+
+
+Select * From dbo.EmployeeDemographics where LastName Like 's%' ;
+
+
+
