71d9cfda6681c117c73d65a6f89138901934efa6
Nawreen Afaz Chaity <nawreen11@gmail.com>
2025-06-13 22:58:24 +0100

    Update Where Statement.sql

--- Where Statement/Where Statement.sql
+++ Where Statement/Where Statement.sql
@@ -33,4 +33,19 @@ Select * From dbo.EmployeeDemographics where Age >'30' or Gender = 'Female';
 Select * From dbo.EmployeeDemographics where LastName Like 's%' ;
 
 
+Select * From dbo.EmployeeDemographics where LastName Like '%s%' ;
+
+
+
+Select * From dbo.EmployeeDemographics where LastName is NULL;
+
+
+
+Select * From dbo.EmployeeDemographics where LastName is Not NULL;
+
+
+
+Select *From dbo.EmployeeDemographics where FirstName In ('Pam' , 'Jim');
+
+
 
